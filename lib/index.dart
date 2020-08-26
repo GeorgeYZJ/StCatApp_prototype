8b7979869b6c0e243b2b4c9293cce586a3338bd3
Marketing Intern <marketingintern@stcatsadm.local>
2020-08-26 15:47:04 +0800

    settings

--- lib/index.dart
+++ lib/index.dart
@@ -15,25 +15,25 @@ class _IndexPageState extends State<IndexPage> {
         title: Text('St Cathrine\'s Home'),
       ),
       body: ListView.builder(
-          itemCount: 100,
-          itemBuilder: (context, index) {
-            return Card(
-              color: index % 2 == 0 ? Color.fromRGBO(218, 170, 0, 0.5) : null,
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  children: [
-                    Text(
-                      'Home Notification ${index + 1}',
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    Text('哈哈哈哈哈哈哈哈哈'),
-                  ],
-                ),
+        itemCount: 100,
+        itemBuilder: (context, index) {
+          return Card(
+            color: index % 2 == 0 ? Color.fromRGBO(218, 170, 0, 0.5) : null,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Text(
+                    'Home Notification ${index + 1}',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  Text('哈哈哈哈哈哈哈哈哈'),
+                ],
               ),
-            );
-          }),
+            ),
+          );
+        },
+      ),
     );
   }
 }
--- lib/setting.dart
+++ lib/setting.dart
@@ -8,32 +8,135 @@ class SettingPage extends StatefulWidget {
 }
 
 class _SettingPageState extends State<SettingPage> {
+  bool _switch1 = false;
+  bool _switch2 = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Setting'),
-      ),
-      body: ListView.builder(
-          itemCount: 100,
-          itemBuilder: (context, index) {
-            return Card(
-              color: index % 2 == 0 ? Color.fromRGBO(218, 170, 0, 0.5) : null,
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
+        backgroundColor: Colors.grey.shade200,
+        appBar: AppBar(
+          title: Text('Setting'),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                margin: const EdgeInsets.all(8),
+                color: Colors.blue,
+                child: ListTile(
+                  title: Text(
+                    'George Yang',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  leading: CircleAvatar(
+                    backgroundColor: Colors.amber,
+                  ),
+                  trailing: Icon(
+                    Icons.edit,
+                    color: Colors.white,
+                  ),
+                  onTap: () {
+                    //turn to other page
+                  },
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                margin: const EdgeInsets.fromLTRB(15, 16, 15, 8),
                 child: Column(
                   children: [
-                    Text(
-                      'Personal Info Setting ${index + 1}',
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ListTile(
+                      title: Text('Change Language'),
+                      leading: Icon(
+                        Icons.language,
+                        color: Color.fromRGBO(0, 114, 206, 1),
+                      ),
+                      trailing: Icon(Icons.keyboard_arrow_right),
+                      onTap: () {
+                        //turn to other page
+                      },
                     ),
-                    Text('哈哈哈哈哈哈哈哈哈'),
+                    Container(
+                      margin: const EdgeInsets.symmetric(
+                        horizontal: 8,
+                      ),
+                      width: double.infinity,
+                      height: 1,
+                      color: Colors.grey.shade400,
+                    ),
+                    ListTile(
+                      title: Text('Change Theme'),
+                      leading: Icon(
+                        Icons.language,
+                        color: Color.fromRGBO(0, 114, 206, 1),
+                      ),
+                      trailing: Icon(Icons.keyboard_arrow_right),
+                      onTap: () {
+                        //turn to other page
+                      },
+                    ),
+                    Container(
+                      margin: const EdgeInsets.symmetric(
+                        horizontal: 8,
+                      ),
+                      width: double.infinity,
+                      height: 1,
+                      color: Colors.grey.shade500,
+                    ),
+                    ListTile(
+                      title: Text('Coming Soon'),
+                      leading: Icon(
+                        Icons.language,
+                        color: Color.fromRGBO(0, 114, 206, 1),
+                      ),
+                      trailing: Icon(Icons.keyboard_arrow_right),
+                      onTap: () {
+                        //turn to other page
+                      },
+                    ),
                   ],
                 ),
               ),
-            );
-          }),
-    );
+              const SizedBox(height: 10),
+              Text(
+                "Notification Settings",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.indigo),
+              ),
+              SwitchListTile(
+                contentPadding: const EdgeInsets.all(5),
+                value: _switch1,
+                activeColor: Colors.blue,
+                onChanged: (bool val) {
+                  setState(() {
+                    _switch1 = val;
+                  });
+                },
+                title: Text("Received Notification"),
+              ),
+              SwitchListTile(
+                contentPadding: const EdgeInsets.all(5),
+                value: _switch2,
+                activeColor: Colors.blue,
+                onChanged: (bool val) {
+                  setState(() {
+                    _switch2 = val;
+                  });
+                },
+                title: Text("Upcoming Event"),
+              ),
+            ],
+          ),
+        ));
   }
 }
